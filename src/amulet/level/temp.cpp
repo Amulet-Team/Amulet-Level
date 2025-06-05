910287da5427756f81713f96c939d90c3c764f52
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-05 11:31:30 +0100

    Add package outline

---
+++ src/amulet/level/temp.cpp
@@ -0,0 +1,7 @@
+#include <amulet/level/dll.hpp>
+
+namespace Amulet{
+namespace level {
+    AMULET_LEVEL_EXPORT void temp(){}
+}
+}
---
+++ tests/test_amulet_level/test_temp.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class TempTestCase(TestCase):
+    def test_temp(self) -> None:
+        import test_amulet_level

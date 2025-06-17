6f8f5089b45e0f8209e889c718b9177fc9a549fc
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-17 14:32:34 +0100

    Expose level classes in amulet.level.abc (#4)

--- src/amulet/level/abc/__init__.pyi
+++ src/amulet/level/abc/__init__.pyi
@@ -2,16 +2,26 @@ from __future__ import annotations
 
 from amulet.level.abc.chunk_handle import ChunkHandle
 from amulet.level.abc.dimension import Dimension
-from amulet.level.abc.level import Level
+from amulet.level.abc.level import (
+    CompactibleLevel,
+    DiskLevel,
+    Level,
+    LevelMetadata,
+    ReloadableLevel,
+)
 from amulet.level.abc.registry import IdRegistry
 
 from . import chunk_handle, dimension, level, registry
 
 __all__ = [
     "ChunkHandle",
+    "CompactibleLevel",
     "Dimension",
+    "DiskLevel",
     "IdRegistry",
     "Level",
+    "LevelMetadata",
+    "ReloadableLevel",
     "chunk_handle",
     "dimension",
     "level",

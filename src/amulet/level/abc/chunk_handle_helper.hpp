7d107600a2e25d7c9382300b99f2c52174f792c0
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-11-05 10:36:45 +0000

    Revert "Remove redundant export markers"
    This reverts commit 0fd0360ee13b7bc031b64784d1142ff60d7faaa7.

--- src/amulet/level/abc/chunk_handle_helper.hpp
+++ src/amulet/level/abc/chunk_handle_helper.hpp
@@ -47,22 +47,22 @@ private:
 
 public:
     // Does the chunk exist. This is a quick way to check if the chunk exists without loading it.
-    bool exists() override;
+    AMULET_LEVEL_EXPORT bool exists() override;
 
     // Get a unique copy of the chunk data.
     AMULET_LEVEL_EXPORT std::unique_ptr<ChunkT> get_native_chunk(std::optional<std::set<std::string>> component_ids = std::nullopt);
 
     // Get a unique copy of the chunk data.
-    std::unique_ptr<Chunk> get_chunk(std::optional<std::set<std::string>> component_ids = std::nullopt) override;
+    AMULET_LEVEL_EXPORT std::unique_ptr<Chunk> get_chunk(std::optional<std::set<std::string>> component_ids = std::nullopt) override;
 
     // Overwrite the chunk data.
     AMULET_LEVEL_EXPORT void set_native_chunk(const ChunkT&);
 
     // Overwrite the chunk data.
-    void set_chunk(const Chunk&) override;
+    AMULET_LEVEL_EXPORT void set_chunk(const Chunk&) override;
 
     // Delete the chunk from the level.
-    void delete_chunk() override;
+    AMULET_LEVEL_EXPORT void delete_chunk() override;
 };
 
 } // namespace Amulet
--- src/amulet/level/java/dimension.hpp
+++ src/amulet/level/java/dimension.hpp
@@ -37,23 +37,23 @@ private:
 
 public:
     // Destructor
-    ~JavaDimension() override;
+    AMULET_LEVEL_EXPORT ~JavaDimension() override;
 
     // Get the dimension id for this dimension.
     // Thread safe.
-    const DimensionId& get_dimension_id() const override;
+    AMULET_LEVEL_EXPORT const DimensionId& get_dimension_id() const override;
 
     // The editable region of the dimension.
     // Thread safe.
-    std::variant<SelectionBox, SelectionBoxGroup> get_bounds() const override;
+    AMULET_LEVEL_EXPORT std::variant<SelectionBox, SelectionBoxGroup> get_bounds() const override;
 
     // Get the default block for this dimension.
     // Thread safe.
-    const BlockStack& get_default_block() const override;
+    AMULET_LEVEL_EXPORT const BlockStack& get_default_block() const override;
 
     // Get the default biome for this dimension.
     // Thread safe.
-    const Biome& get_default_biome() const override;
+    AMULET_LEVEL_EXPORT const Biome& get_default_biome() const override;
 
     // Get a chunk handle for a specific chunk.
     // Thread safe.
@@ -61,7 +61,7 @@ public:
 
     // Get a chunk handle for a specific chunk.
     // Thread safe.
-    std::shared_ptr<ChunkHandle> get_chunk_handle(std::int64_t cx, std::int64_t cz) override;
+    AMULET_LEVEL_EXPORT std::shared_ptr<ChunkHandle> get_chunk_handle(std::int64_t cx, std::int64_t cz) override;
 };
 
 } // namespace Amulet

5da8fb4639630777d513e0fc64fd1a0f849fae92
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-11-20 15:37:10 +0000

    Update stubs

--- src/amulet/level/bedrock/chunk.pyi
+++ src/amulet/level/bedrock/chunk.pyi
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
 import amulet.core.chunk
+import amulet.core.chunk.component.block_component
 import amulet.level.bedrock.chunk_components
 
-__all__: list[str] = ["BedrockChunk", "BedrockChunkTemp"]
+__all__: list[str] = ["BedrockChunk", "BedrockChunk1"]
 
 class BedrockChunk(amulet.core.chunk.Chunk):
     pass
 
-class BedrockChunkTemp(
-    BedrockChunk, amulet.level.bedrock.chunk_components.BedrockRawChunkComponent
+class BedrockChunk1(
+    BedrockChunk,
+    amulet.level.bedrock.chunk_components.BedrockRawChunkComponent,
+    amulet.core.chunk.component.block_component.BlockComponent,
 ):
     pass
